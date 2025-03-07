353502ec66be6ebb062135896d124caa919f47ce
Muhammad Hussam <hussam@clustox.com>
2025-03-07 16:29:49 +0500

    Implemented basic example of task list in Story book

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: false,
+ template: `
+   <app-inbox-screen></app-inbox-screen>
+ `,
+})
+export class AppComponent {
+  title = 'taskbox';
+}
